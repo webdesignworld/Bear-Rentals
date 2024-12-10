2f9871887cbc26a9eba9b7a3ff02b42056860ff9
Svetlana <web_design_world@outlook.com>
2024-12-10 20:12:53 +0100

    added property photos to db

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -1,12 +1,82 @@
 "use server";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import cloudinary from "@/config/cloudinary";
 
-async function addProprty(formData) {
+async function addProperty(formData) {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required");
+  }
+
+  const { userId } = sessionUser;
+
   const amenities = formData.getAll("amenities");
-  const images = formData
-  .getAll("images")
-  .filter((image) => image.name !== "")
-  .map((image) => image.name);
-  console.log(images)
+  const images = formData.getAll("images").filter((image) => image.name !== "");
+
+  const propertyData = {
+    owner: userId,
+    type: formData.get("type"),
+    name: formData.get("name"),
+    description: formData.get("description"),
+    location: {
+      street: formData.get("location.street"),
+      city: formData.get("location.city"),
+      state: formData.get("location.state"),
+      postcode: formData.get("location.postcode"),
+    },
+    beds: formData.get("beds"),
+    baths: formData.get("baths"),
+    square_meters: formData.get("square_meters"),
+    amenities,
+    rates: {
+      nightly: formData.get("rates.nightly"),
+      weekly: formData.get("rates.weekly"),
+      monthly: formData.get("rates.monthly"),
+    },
+    seller_info: {
+      name: formData.get("seller_info.name"),
+      email: formData.get("seller_info.email"),
+      phone: formData.get("seller_info.phone"),
+    },
+  };
+
+  const imageUrls = [];
+
+  for (const imageFile of images) {
+    const imageBuffer = await imageFile.arrayBuffer();
+    const imageArray = Array.from(new Uint8Array(imageBuffer));
+    const imageData = Buffer.from(imageArray);
+
+    // Convert the image data to base64
+    const imageBase64 = imageData.toString("base64");
+
+    // Make request to upload to Cloudinary
+    const result = await cloudinary.uploader.upload(
+      `data:image/png;base64,${imageBase64}`,
+      {
+        folder: "BearRentals",
+      }
+    );
+
+    imageUrls.push(result.secure_url);
+  }
+
+  propertyData.images = imageUrls;
+
+  // console.log(propertyData);
+
+  const newProperty = new Property(propertyData);
+  await newProperty.save();
+
+  revalidatePath("/", "layout");
+  redirect(`/properties/${newProperty._id}`);
 }
 
-export default addProprty;
+export default addProperty;
