85fecf8d97bd36f3c77a6823e9f3a627aed68039
Svetlana <web_design_world@outlook.com>
2025-03-18 14:48:16 +0100

    added photoswipe for react images listing

--- app/components/PropertyImages.jsx
+++ app/components/PropertyImages.jsx
@@ -1,38 +1,64 @@
 "use client";
+
 import Image from "next/image";
+import { Gallery, Item } from "react-photoswipe-gallery";
 
 const PropertyImages = ({ images }) => {
   return (
-    <section className="bg-blue-50 p-4">
-      <div className="container mx-auto">
-        {images.length === 1 ? (
-          <Image
-            src={images[0]}
-            alt=""
-            className="object-cover h-[400px] mx-auto rounded-xl"
-            width={1800}
-            height={400}
-            priority={true}
-          />
-        ) : (
-          <div className="grid grid-cols-2 gap-4">
-            {images.map((image, index) => (
-              <div key={index} className="col-span-1">
+    <Gallery>
+      <section className="bg-blue-50 p-4">
+        <div className="container mx-auto">
+          {images.length === 1 ? (
+            <Item
+              original={images[0]}
+              thumbnail={images[0]}
+              width="1000"
+              height="600"
+            >
+              {({ ref, open }) => (
                 <Image
-                  src={image}
+                  src={images[0]}
+                  ref={ref}
+                  onClick={open}
                   alt=""
-                  className="object-cover h-[400px] w-full rounded-xl"
+                  className="object-cover h-[400px] mx-auto rounded-xl cursor-pointer"
                   width={1800}
                   height={400}
-                  sizes="100vw"
                   priority={true}
                 />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+              )}
+            </Item>
+          ) : (
+            <div className="grid grid-cols-2 gap-4">
+              {images.map((image, index) => (
+                <div key={index} className="col-span-1">
+                  <Item
+                    original={image}
+                    thumbnail={image}
+                    width="1000"
+                    height="600"
+                  >
+                    {({ ref, open }) => (
+                      <Image
+                        src={image}
+                        ref={ref}
+                        onClick={open}
+                        alt=""
+                        className="object-cover h-[400px] w-full rounded-xl cursor-pointer"
+                        width={1800}
+                        height={400}
+                        sizes="100vw"
+                        priority={true}
+                      />
+                    )}
+                  </Item>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </Gallery>
   );
 };
 export default PropertyImages;
--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import AuthProvider from "./components/AuthProvider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
 import { ThemeProvider } from "./components/providers/ThemeProvider";
+import 'photoswipe/dist/photoswipe.css'
 
 export const metadata = {
   title: 'Bear Rentals',
